fa4d00efa853819e3a3c49951f3a107cab699679
OSAMAMOHAMED1234 <om1234@fayoum.edu.eg>
2018-02-27 22:40:41 +0200

    group first name and last name in full name by jquery more dynamic code

--- static/js/add_full_name.js
+++ static/js/add_full_name.js
@@ -1,14 +1,44 @@
 $(function () {
-    $('input[name=first_name]').on('keyup', function () {
-        $('input[name=full_name]').val($(this).val());
-    }, function () {
-        $('input[name=last_name]').on('keyup', function () {
-            $('input[name=full_name]').val($('input[name=first_name]').val() + ' ' + $('input[name=last_name]').val());
-        });
+    var firstName = '';
+    var lastName = '';
+    $('input[name=first_name]').on('keyup keypress blur change', function () {
+        firstName = $(this).val();
+        updateFullName()
     });
-    // var lastName = '';
-    // $('input[name=last_name]').keyup(function () {
-    //     var lastName = $('input[name=last_name]').val();
-    //     $('input[name=full_name]').val(firstName + lastName);
+    $('input[name=last_name]').on('keyup keypress blur change', function () {
+        lastName = $(this).val();
+        updateFullName()
+    });
+    function updateFullName() {
+        $('input[name=full_name]').val([firstName + ' ' + lastName]);
+    }
+
+
+    // var vals = ['', ''];
+    // $('input[name="first_name"]').on('keyup keypress blur change', function(){
+    //   vals[0] = $(this).val();
+    //   updateFullName();
     // });
+    // $('input[name="last_name"]').on('keyup keypress blur change', function(){
+    //   vals[1] = $(this).val();
+    //   updateFullName();
+    // });
+    // function updateFullName(){
+    //   $('input[name="full_name"]').val( vals.join(' ') );
+    // }
+
+
+
+    // $('input[name=first_name]').on('keyup keypress blur change', function () {
+    //     // $('input[name=full_name]').val($(this).val());
+    // }, function () {
+    //     $('input[name=last_name]').on('keyup keypress blur change', function () {
+    //         var lastName = $('input[name=last_name]').val();
+    //         $('input[name=full_name]').val($('input[name=first_name]').val() + ' ' + lastName);
+    //         // $('input[name=full_name]').val($('input[name=first_name]').val() + ' ' + $('input[name=last_name]').val());
+    //     });
+    // });
+
 });
+
+
